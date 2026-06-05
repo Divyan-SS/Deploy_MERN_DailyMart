84c6370e3f3d8ef49fedb886336acfb53fda76af
Divyan-SS <divyan9629@gmail.com>
2026-06-05 11:20:57 +0530

    fix: force IPv4 connection in nodemailer transporter to prevent ENETUNREACH on IPv6

--- backend/config/mail.js
+++ backend/config/mail.js
@@ -2,6 +2,7 @@ import nodemailer from 'nodemailer';
 
 export const transporter = nodemailer.createTransport({
   service: 'gmail',
+  family: 4, // Force connection over IPv4 to prevent ENETUNREACH on IPv6 routing
   auth: {
     user: process.env.SMTP_EMAIL,
     pass: process.env.SMTP_PASSWORD,
